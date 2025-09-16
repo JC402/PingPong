d9a5b481c49c36ef2909bac67146935b65a536b4
JC402 <jarsenault@rollins.edu>
2025-09-15 22:01:24 -0400

    Updated

---
+++ Ping Pong/Assets/Scripts/LeftPaddleController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class LeftPaddleController : MonoBehaviour
+{
+    Rigidbody2D pad;
+    Vector2 initial;
+    public float displacement;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        pad = GetComponent<Rigidbody2D>();
+        initial = pad.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.Q))) {
+            if (initial.y <= 9.75)
+                initial.y += displacement;
+        } else if((Input.GetKey(KeyCode.A))) {
+            if (initial.y > -9.75)
+                initial.y -= displacement;
+        }
+        pad.MovePosition(initial);
+    }
+}
